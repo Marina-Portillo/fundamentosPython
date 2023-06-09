759e4715fc8217d425afc7d4ce26006ad0911181
Marina <maryypucca@gmail.com>
2023-06-09 16:20:42 -0600

    02 variables

---
+++ fundamentosPayton/variable.py
@@ -0,0 +1,15 @@
+#python es no tipado
+
+name ="Francisco"
+last_name ="Lopez"
+age = 39
+height=1.68
+is_student= False
+groups=["3A","3B","3C"]
+
+#function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
